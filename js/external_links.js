0966002ffb1bd46dfee307e0952e28ea291c24d9
Robert Bowdidge <robert.bowdidge@fungible.com>
2022-12-03 16:07:20 -0800

    Try complete rewrite.

---
+++ js/external_links.js
@@ -0,0 +1,24 @@
+﻿addLoadEvent(externalLinks);
+
+function externalLinks() {
+  if (!document.getElementsByTagName) return;
+  var anchors = document.getElementsByTagName("a");
+  for (var i=0; i<anchors.length; i++) {
+    var anchor = anchors[i];
+    if (anchor.getAttribute("href") &&
+      anchor.getAttribute("rel") == "external")
+      anchor.target = "_blank";
+    }
+}
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
